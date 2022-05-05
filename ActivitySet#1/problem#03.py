9f3923cfc23f4868bc1ba55b021919b46d28ee93
Yashas Kishore <95675075+Yashas-Kishore@users.noreply.github.com>
2022-05-05 17:46:30 +0000

    act#1, prbm2,prbm3

--- ActivitySet#1/problem#03.py
+++ ActivitySet#1/problem#03.py
@@ -1,3 +1,7 @@
 # Variables, Expressions & Statements
 
 hrs = float(input("Enter hours? "))
+rate = input('enter the rate per hour:')
+payment = float(hrs) * float(rate)
+print('payment to made for the respective hrs is:',payment)
+
