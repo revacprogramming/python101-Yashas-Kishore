c8685fc56624a215690bf3e84f6e93dd2578126d
Yashas Kishore <95675075+Yashas-Kishore@users.noreply.github.com>
2022-05-07 17:14:53 +0000

    ActivitySet#1/problem#04

--- ActivitySet#1/problem#04.py
+++ ActivitySet#1/problem#04.py
@@ -1,5 +1,5 @@
 # Conditional Execution
-
+'''
 hrs = input("Enter Hours:")
 h = float(hrs)
 rtph = input("enter the rate per hour:")
@@ -16,5 +16,38 @@ else :
     
 ptm = (h*rt) + (ovt*(rt*1.5))
 print(ptm)
-    
-
+'''
+'''
+score = input("Enter Score: ")
+sc = float(score)
+if sc > 0.0 and sc < 1.0 :
+    if sc >= 0.9:
+        print('A')
+    elif sc >= 0.8:
+        print('B')
+    elif sc  >= 0.7:
+        print('C')
+    elif sc >= 0.6:
+        print('D')
+    else:
+        print('F')
+else :
+    print("enter valid marks within 0.0 to 1.0")
+'''
+score = input("Enter Score: ")
+sc = float(score)
+try :
+    sc > 0.0 and sc < 1.0
+    if sc >= 0.9:
+        print('A')
+    elif sc >= 0.8:
+        print('B')
+    elif sc >= 0.7:
+        print('C')
+    elif sc >= 0.6:
+        print('D')
+    else:
+        print('F')
+except :
+    print("enter valid marks within 0.0 to 1.0")
+  
