06dcdb3483b1e3645d57d408147198d77515baf5
Yashas Kishore <95675075+Yashas-Kishore@users.noreply.github.com>
2022-05-07 08:58:12 +0000

    ActivitySet#1/problem#04.py

--- ActivitySet#1/problem#04.py
+++ ActivitySet#1/problem#04.py
@@ -1,3 +1,20 @@
 # Conditional Execution
 
-hrs = input("Enter hours? ")
+hrs = input("Enter Hours:")
+h = float(hrs)
+rtph = input("enter the rate per hour:")
+rt = float(rtph)
+if h > 40 :
+  print('overtime')
+  ovt = h-40
+  print(ovt,'hrs')
+  h = 40
+else :
+  print ("regular")
+  h = h + 0
+  ovt = 0
+    
+ptm = (h*rt) + (ovt*(rt*1.5))
+print(ptm)
+    
+
