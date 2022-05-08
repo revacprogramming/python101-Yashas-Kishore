7c1731d6c5f026a032e4d29c4a1c25fcf87497c8
Yashas Kishore <95675075+Yashas-Kishore@users.noreply.github.com>
2022-05-08 14:17:45 +0000

    05

--- ActivitySet#1/problem#05.py
+++ ActivitySet#1/problem#05.py
@@ -1,12 +1,21 @@
 # Functions
 
 
-def computepay(h, r):
-    pass  # ...
-
-
-hrs = float(input("Enter hours? "))
-rte = float(input("Enter rate per hour? "))
-
-p = computepay(hrs, rte)
+def computepay(hr, r):
+    h = float(hr)
+    
+    rt = float(r)
+    if h > 40 :
+        # print('overtime')
+        ovt = h-40
+        h = 40
+    else :
+        # print ("regular")
+        h = h + 0
+        ovt = 0
+    ptm = (h*rt) + (ovt*(rt*1.5))
+    return(ptm)
+hrs = input("Enter Hours:")
+rtph = input("enter the rate per hour:")
+p = computepay(hrs,rtph)
 print("Pay", p)
