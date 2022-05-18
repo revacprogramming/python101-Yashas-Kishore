00f1a3c85431128f8dc0d9220df174d180ba94f5
Yashas Kishore <95675075+Yashas-Kishore@users.noreply.github.com>
2022-05-18 02:29:56 +0000

    ljkut

--- ActivitySet#1/problem#06.py
+++ ActivitySet#1/problem#06.py
@@ -1,21 +1,51 @@
 # Loops & Iterators
-
+'''
 largest = none
 smallest = none
 
 while True:
-    num = input("Enter a number: ")
-    if num == "done":
-       break
-    try:
-        x=int(num)
-        if largest is none or largest<x:
-            largest=x
-        if smallest  is none or smallest>x:
-            smallest=x
+  num = input("Enter a number: ")
+  if num == "done":
+    break
+  try:
+    x=int(num)
+    if largest is none or largest<x:
+      largest=x
+    if smallest  is none or smallest>x:
+      smallest=x
 	except:
-        print("invalid input")
+    print("invalid input")
         
       
 print("Maximum is", largest)       
+print("Minimum is",smallest)
+'''
+largest = None
+smallest = None
+while True:
+    num = input("Enter a number: ")
+    
+    if num == "over":
+        break
+    try :
+        a = int(num)
+        
+        if largest is None:
+            largest = a
+        elif largest>a :
+            largest = largest
+        else :
+            largest=a
+        if smallest is None:
+            smallest= a
+        elif smallest < a:
+            smallest= smallest
+        else:
+            smallest= a      
+   
+       
+    except:
+        print ("Invalid input")
+        
+print("Maximum is", largest)
 print("Minimum is",smallest)
--- ActivitySet#1/problem#08.py
+++ ActivitySet#1/problem#08.py
@@ -1,3 +1,21 @@
 # Files
 
-filename = "dataset/mbox-short.txt"
+#filename = "dataset/mbox-short.txt"
+# Use the file name mbox-short.txt as the file name
+fname = input("Enter file name: ")
+fh = open(fname)
+avg=0
+count=0
+for line in fh:
+    if not line.startswith("X-DSPAM-Confidence:"):
+        continue
+    test=line.find(':')
+    toadd=line[test+1 : ]
+    fcopy=float(toadd)
+    avg=avg+fcopy
+    count=count+1
+    #print(fcopy)
+    #print(line)
+result=avg/count
+print("Average spam confidence:",result)
+
