34367e9349705aa513786ffc13a60be387ac156c
Yashas Kishore <95675075+Yashas-Kishore@users.noreply.github.com>
2022-05-23 16:23:09 +0000

    jmjh

--- ActivitySet#1/problem#04.py
+++ ActivitySet#1/problem#04.py
@@ -1,21 +1,6 @@
 # Conditional Execution
 '''
-hrs = input("Enter Hours:")
-h = float(hrs)
-rtph = input("enter the rate per hour:")
-rt = float(rtph)
-if h > 40 :
-  print('overtime')
-  ovt = h-40
-  print(ovt,'hrs')
-  h = 40
-else :
-  print ("regular")
-  h = h + 0
-  ovt = 0
-    
-ptm = (h*rt) + (ovt*(rt*1.5))
-print(ptm)
+
 '''
 '''
 score = input("Enter Score: ")
---
+++ ActivitySet#1/problem#18.py
@@ -0,0 +1,16 @@
+hrs = input("Enter Hours:")
+h = float(hrs)
+rtph = input("enter the rate per hour:")
+rt = float(rtph)
+if h > 40 :
+  print('overtime')
+  ovt = h-40
+  print(ovt,'hrs')
+  h = 40
+else :
+  print ("regular")
+  h = h + 0
+  ovt = 0
+    
+ptm = (h*rt) + (ovt*(rt*1.5))
+print(ptm)
