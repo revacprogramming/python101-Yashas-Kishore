9bad24ef362ea79bc295138f2d9080ae0f5447d1
Yashas Kishore <95675075+Yashas-Kishore@users.noreply.github.com>
2022-06-13 09:50:29 +0000

    qerkuy

--- ActivitySet#1/problem#19.py
+++ ActivitySet#1/problem#19.py
@@ -1,3 +1,4 @@
+'''
 import urllib.request,urllib.parse,urllib.error
 from bs4 import BeautifulSoup
 import ssl
@@ -12,7 +13,7 @@ html = urllib.request.urlopen(url, context=ctx).read()
 searches= BeautifulSoup(html, "html.parser")
 a='span'
 #tags=search('a')
-'''
+
 for tag in test:
     print('TAG: ',tag)
     if tag == tag.contents[0]:
@@ -20,7 +21,7 @@ for tag in test:
       
    # if tag == span:
     #    print('url:',tag.get('span',None)+1)
-'''
+
 tags = soup('a')
 for tag in tags:
     # Look at the parts of a tag
@@ -28,3 +29,40 @@ for tag in tags:
     print('URL:', tag.get('href', None))
     print('Contents:', tag.contents[0])
     print('Attrs:', tag.attrs)
+http://py4e-data.dr-chuck.net/comments_42.html
+'''
+
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+import ssl
+
+# Ignore SSL certificate errors
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
+
+# url = input('Enter - ')
+url = "http://py4e-data.dr-chuck.net/comments_42.html"
+reqs = urlopen(url, context=ctx)
+html = reqs.read()
+soup = BeautifulSoup(html, "html.parser")
+
+# Retrieve all of the anchor tags
+tags = soup('span')
+# print(html.getcode())
+# print(html.read())
+# print(html)
+# print(tags)
+lst=list()
+for tag in tags:
+    # Look at the parts of a tag
+    #print('TAG:', tag)
+    #print('URL:', tag.get('href', None))
+    
+    Contents= tag.contents[0]
+    nums=int(Contents)
+    lst.append(nums)
+print(sum(lst))
+
+# <br />
+# <br></br>
