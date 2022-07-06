504370f03365be3d1d81023e0ebf13cf0c6d7cef
Yashas-Kishore <95675075+Yashas-Kishore@users.noreply.github.com>
2022-07-06 09:37:30 +0530

    Update problem#5.py

--- ActivitySet#2/problem#5.py
+++ ActivitySet#2/problem#5.py
@@ -1,26 +1,26 @@
-
-
 def get_cs():
-    """get string input"""
-
-
+	string=input("enter the string for the process:")
+	return string
 def cs_to_dict(cs):
-    """convert connect string to a dictionary"""
-
-
+	d=dict()
+	split1=cs.split(';')
+	for i in split1:
+		split2=i.split('=')
+		d[split2[0]]=split2[1]
+	return d
 def dict_to_cs(d):
-    """convert a dictionary to connect string"""
-
-
+	x=list()
+	for i in d:
+		join1=i+'='+d[i]
+		x.append(join1)
+	y=";".join(x)
+	return y
 def main():
-    cs = get_cs()
-
-    d = cs_to_dict(cs) # convert connect string to a dictionary
-    print(d)
-
-    cs = dict_to_cs(d)
-    print(cs)
-
-
-if __name__ == '__main__':
-    main()
+	cs=get_cs()
+	d=cs_to_dict(cs)
+	print(d)
+	cs=dict_to_cs(d)
+	print(cs)
+	
+if _name_ == '_main_':
+	main()
