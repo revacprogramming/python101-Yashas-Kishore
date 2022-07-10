24d1459a1ec05b4995480faf93823e58b5c3d840
Yashas-Kishore <yashaskgpersonal01@gmail.com>
2022-07-10 17:38:20 +0530

    5

--- ActivitySet#2/problem#5.py
+++ ActivitySet#2/problem#5.py
@@ -2,14 +2,26 @@
 
 def get_cs():
     """get string input"""
+    str=input('enter the shortcuts and its reps in form "word=sf" \n')
+    return str
 
 
 def cs_to_dict(cs):
-    """convert connect string to a dictionary"""
-
+    word=cs.split(';')
+    y=dict()
+    for test in word:
+        z=test.split('=')
+        y[z[0]]=z[1]
+        
+    return y
 
 def dict_to_cs(d):
-    x='test'
+    m=list()
+    for i in d:
+        x=i+d[i]
+        m.append(x)
+    test=";".join(m)
+    return test
 
 
 def main():
@@ -20,7 +32,7 @@ def main():
     d = cs_to_dict(cs) # convert connect string to a dictionary
     print(d)
 
-    cs = dict_to_cs(d)
+    dict_to_cs(d)
     print(cs)
 
 
