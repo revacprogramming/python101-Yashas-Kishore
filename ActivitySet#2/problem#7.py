35a694180df56b9ce2db44fca970ce790fc2e10b
Yashas-Kishore <yashaskgpersonal01@gmail.com>
2022-07-15 23:13:54 +0530

    SDVzx

--- ActivitySet#2/problem#7.py
+++ ActivitySet#2/problem#7.py
@@ -1,10 +1,23 @@
 
 
 class Menu:
-    """fill in class definition here"""
+    
+    def __init__(self):
+        self.items={}
+        
+    
+    def __add__(self,item,rate):
+        self.item = item
+        self.rate = rate
+        self.items[self.item]=self.rate
+
+
+    def show(self):
+        for i in self.x:
+            print(i,' ',self.x[i])
 
 
 m = Menu()
-m = m + ("idly", 10) + ("vada", 20)  # Hint: operator overloading special methods (__add__, __sub__, etc.)
+m = m + ("idly" , 10) + ("vada" , 20)  # Hint: operator overloading special methods (__add__, __sub__, etc.)
 
 print(m)  # should print the menu properly
