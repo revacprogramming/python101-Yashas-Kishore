799dfa7b00b461da5b73c858378eac2296f263a2
Yashas-Kishore <95675075+Yashas-Kishore@users.noreply.github.com>
2022-07-24 21:58:33 +0530

    Update problem#8.py

--- ActivitySet#2/problem#8.py
+++ ActivitySet#2/problem#8.py
@@ -1,8 +1,18 @@
 
-
 class Menu:
     """fill in class definition here"""
+    def __init__(self) -> None:
+        self.foods={}
+        
+        pass
+    def __setitem__(self,item,price):
+        self.foods[item]=price
 
+    def __str__(self):
+        s=""    
+        for i in self.foods:
+            s+=(f"{i}  {self.foods[i]}\n")
+        return (s[:-1])
 
 m = Menu()
 m["idly"] = 10
